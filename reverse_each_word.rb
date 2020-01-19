04554af94fdeac1f0d75e691fe28184cbc478c7d
Daryl Corbin <corbindac@gmail.com>
2020-01-19 17:48:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,30 @@
+def reverse_each_word(str)
+str1 = str.split(" ")
+# splitting the sentence on the basis of space and storing it into a variable
+ans = []
+# answer array to store result
+# iterating over elements of array using .each
+str1.each do |i|
+# pushing reversed word into answer array
+ans.push(i.reverse)
+end
+# returning answer array after joing each item with a space
+return ans.join(' ')
+end
+print(reverse_each_word("Hello there, and how are you?"))
+
+# function to reverse each word in a sentence string
+def reverse_each_word(str)
+str1 = str.split(" ")
+# splitting the sentence on the basis of space and storing it into a variable
+ans = []
+# answer array to store result
+# iterating over elements of array using .collect
+str1.collect do |i|
+# pushing reversed word into answer array
+ans.push(i.reverse)
+end
+# returning answer array after joing each item with a space
+return ans.join(' ')
+end
+print(reverse_each_word("Hello there, and how are you?"))
